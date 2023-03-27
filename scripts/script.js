f53ef516ccbdbb8f5bf7c4a6850376707e432b2d
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-27 16:16:58 -0700

    event bubbling green color

--- scripts/script.js
+++ scripts/script.js
@@ -1,38 +1,56 @@
-const btn = document.querySelector("#clickMe")
+// const btn = document.querySelector("#clickMe")
 
-function alertUser()
-{
-    alert("You Clicked!!!");
-    // btn.removeEventListener("click", alertUser);
-};
+// function alertUser()
+// {
+//     alert("You Clicked!!!");
+//     // btn.removeEventListener("click", alertUser);
+// };
 
-function changeBackground()
-{
-   document.body.classList.add("pink_background")
-}
+// function changeBackground()
+// {
+//    document.body.classList.add("pinkBG")
+// }
 
-function textChange()
+// function textChange()
+// {
+//     if (btn.textContent === "Click Me!!!")
+//     {
+//         btn.textContent = "Clicked!";
+//     }
+//     else {
+//         btn.textContent = "Click Me!!!";
+//     }
+// };
+
+// btn.addEventListener("click", alertUser)
+// btn.addEventListener("click", changeBackground)
+// btn.addEventListener("click", textChange)
+
+// function updateImage(event)
+// {
+//     console.log(event);
+//     const image = document.querySelector("#shoppingCart");
+//     image.setAttribute("src", "image/263142.png");
+//     image.setAttribute("alt", "shopping cart icon");
+//     image.setAttribute("width", "50");
+//     image.setAttribute("height", "50");
+// }
+
+// btn.addEventListener("click", updateImage);
+
+const btnContainer = document.querySelector(".button-container");
+
+function changeBGGreen(event)
 {
-    if (btn.textContent === "Click Me!!!")
+    if (event.target.tagName === "BUTTON")
     {
-        btn.textContent = "Clicked!";
+        event.target.classList.add("greenBG")
     }
-    else {
-        btn.textContent = "Click Me!!!";
+    else
+    {
+        event.target.classList.add("aquaBG")
     }
-};
-
-btn.addEventListener("click", alertUser)
-btn.addEventListener("click", changeBackground)
-btn.addEventListener("click", textChange)
-
-function updateImage()
-{
-    const image = document.querySelector("#shoppingCart");
-    image.setAttribute("src", "image/263142.png");
-    image.setAttribute("alt", "shopping cart icon");
-    image.setAttribute("width", "50");
-    image.setAttribute("height", "50");
 }
 
-btn.addEventListener("click", updateImage);
+// btnContainer.classList.add("aquaBG");
+btnContainer.addEventListener("click", changeBGGreen);
