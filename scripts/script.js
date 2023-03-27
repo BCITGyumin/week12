989a71573e617cb8434c52851a29a74a792fd9f1
Gyumin Kim <gyuminkim2002@gmail.com>
2023-03-27 15:55:12 -0700

    updateImage event listener

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,14 @@ function textChange()
 btn.addEventListener("click", alertUser)
 btn.addEventListener("click", changeBackground)
 btn.addEventListener("click", textChange)
+
+function updateImage()
+{
+    const image = document.querySelector("#shoppingCart");
+    image.setAttribute("src", "image/263142.png");
+    image.setAttribute("alt", "shopping cart icon");
+    image.setAttribute("width", "50");
+    image.setAttribute("height", "50");
+}
+
+btn.addEventListener("click", updateImage);
